410af950b3364659a9a2259c8ef9606d03226935
KevinTulakyan <KevinTulakyan@gmail.com>
2021-01-31 17:24:00 -0800

    youtube ssearch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,20 @@
-async function handleSubmit(event) {
-    event.preventDefault();
-    let inputValue = document.querySelector(".js-search-input").value;
-    let searchQuery = inputValue.trim();
+let apiKey = "AIzaSyBElNgf8IN6XlOh7RzTdzE_DfEnNXGjBDI";
+let display= "";
 
-    let searchResults = document.querySelector(".js-search-results");
-    searchResults.innerHTML = '';
+$(document).ready(function () {
+    $("#form").submit(function (event) {
+        event.preventDefault();
+        let youSearch = $("#search").val();
+        videoSearch(apiKey, youSearch,2)
+    })
 
-    let spinner = document.querySelector('.js-spinner');
-    spinner.classList.remove('hidden');
-
-    try {
-        let results = await searchWikipedia(searchQuery);
-        if (results.query.searchinfo.totalhits === 0) {
-            alert('No results found. Try different keywords');
-            return;
-        }
-
-        displayResults(results);
-    }
-    finally {
-        spinner.classList.add('hidden');
-    }
-}
-
-async function searchWikipedia(searchQuery) {
-    const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${searchQuery}`;
-    const response = await fetch(endpoint);
-    if (!response.ok) {
-      throw Error(response.statusText);
-    }
-    const json = await response.json();
-    return json;
-}
-
-function displayResults(results) {
-        let searchResults = document.querySelector('.js-search-results');
-
-        results.query.search.forEach(result => {
-            let url = `https://en.wikipedia.org/?curid=${result.pageid}`;
-            searchResults.insertAdjacentHTML('beforeend', `<div class="result-item"><h3 class="result-title"><a href="${url}" target="_blank" rel="noopener">${result.title}</a></h3><span class="result-snippet">${result.snippet}</span><br></div>`)
+    function videoSearch(key,video,maxVideo) {
+        $.get("https://www.googleapis.com/youtube/v3/search?key="+key+"&type=video&part=snippet&maxResults="+maxVideo+"&q="+video,function(data){
+            
+            data.items.forEach(item => {
+                video=`<iframe width="560" height="315" src="https://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allowfullscreen></iframe>`
+            });
+            $("#video").append(video);
         })
-}
-
-
-
-    let form = document.querySelector('.js-search-form');
-    form.addEventListener('submit', handleSubmit);
+    }
+})
